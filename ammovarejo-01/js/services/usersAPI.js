86b7ccac7f043045b33f5cad42403b5012cb15d2
Antonio <antonio@titaniumtech.com.br>
2017-01-05 17:06:45 -0200

    Teste para Vaga Front-end AmmoVarejo

---
+++ ammovarejo-01/js/services/usersAPI.js
@@ -0,0 +1,9 @@
+angular.module('ammovarejo').factory('usersAPI', function($http){
+	var _getUsers = function(){
+		return $http.get('http://localhost:3412/users');
+	}
+
+	return {
+		getUsers: _getUsers
+	}
+});
